8b5f8357b3404bad097db2a9bf8946be22e1649f
SantiagoGG0 <santiagogarcia337@gmail.com>
2023-05-16 20:48:34 -0500

    Creación del controlador y las rutas para el profesor

--- src/app/controllers/TeacherController.ts
+++ src/app/controllers/TeacherController.ts
@@ -1,1 +1,32 @@
- 
+import { Teacher } from './../../domain/entities/Teacher';
+import { ITeacherRepository } from "domain/interfaces/ITeacherRepository";
+import { Request, Response } from 'express';
+// LA API PUEDE SER MEJORADA POR MEDIO DE VALIDACIONES.
+ export class TeacherController {
+
+    constructor( private _teacherRepository: ITeacherRepository ){
+    }
+    //Asincronía y sincronía. (Consultar.)
+    public async create( request:Request, response: Response ): Promise<Response> {
+        try {
+            const {name, description, email, birthDate} = request.body;
+            const teacher: Teacher = new Teacher(name, description, email, new Date(birthDate));
+            const createdTeacher = await this._teacherRepository.create(teacher);
+    
+            return response.status(201).json( createdTeacher )
+        }
+        
+         catch(error){
+        // EL error se utiliza ahi a modo de ejemplo no se puede mostrar en una empresa.
+        console.log(error)
+        return response.status(500).json({
+            errorMessage: 'Houston tenemos un problema!',
+            code: 99999
+        });
+
+
+
+        }
+      
+    }
+ }
--- src/app/routes/TeacherRoutes.ts
+++ src/app/routes/TeacherRoutes.ts
@@ -1,1 +1,18 @@
- 
+import { TeacherController } from "app/controllers/TeacherController";
+import { TeacherRepository } from "infra/repositories/TeacherRepository";
+import { Router } from "express"
+
+ export class TeacherRoutes {
+
+    private _router: Router;
+    private _teacherRepository: TeacherRepository;
+    private _teacherController: TeacherController;
+
+    //PRINCIPIOS SOLID.
+
+    constructor(){
+        this._router = Router();
+        this._teacherRepository = new TeacherRepository();
+        this._teacherController = new TeacherController();
+    }
+ }
